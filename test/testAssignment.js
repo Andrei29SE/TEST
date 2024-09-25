27c8672238c58ea86574670047291e5931bd95b7
Andrei Skiruta <skiruta.andrei@mail.ru>
2024-09-25 12:39:17 +0700

    2

--- test/testAssignment.js
+++ test/testAssignment.js
@@ -1,31 +1,109 @@
+// const { Builder, By, Key, until } = require('selenium-webdriver')
+
+// const chrome = require('selenium-webdriver/chrome')
+
+// async function getWeather(city) {
+//   let driver = await new Builder().forBrowser('chrome').build()
+//   await driver.get(`https://www.google.com/search?q=weather+${city}`)
+
+//   let temperature = await driver
+//     .wait(until.elementLocated(By.id('wob_tm')))
+//     .getText()
+//   let condition = await driver
+//     .wait(until.elementLocated(By.id('wob_dc')))
+//     .getText()
+//   await driver.quit()
+
+//   return `${city}: ${temperature}°C, ${condition}`
+// }
+
+// async function main() {
+//   let cities = ['London', 'Tokyo', 'Washington D.C.', 'Canberra', 'Brasília']
+//   let weatherResults = []
+
+//   for (let city of cities) {
+//     let weather = await getWeather(city)
+//     weatherResults.push(weather)
+//   }
+
+//   console.log(weatherResults)
+// }
+
+// main()
+
 const { Builder, By, Key, until } = require('selenium-webdriver')
 const chrome = require('selenium-webdriver/chrome')
+const sleep = require('util').promisify(setTimeout) // Sleep function
 
-async function getWeather(city) {
-  let driver = await new Builder().forBrowser('chrome').build()
-  await driver.get(`https://www.google.com/search?q=weather+${city}`)
+// Constants (replace with your Multilogin credentials)
+const MULTILOGIN_URL = 'https://app.multilogin.com/en/home/profiles'
+const USERNAME = 'skiruta.andrei@mail.ru' // TODO: Add your Multilogin username
+const PASSWORD = 'Andrei2904.' // TODO: Add your Multilogin password
 
-  let temperature = await driver
-    .wait(until.elementLocated(By.id('wob_tm')), 5000)
-    .getText()
-  let condition = await driver
-    .wait(until.elementLocated(By.id('wob_dc')), 5000)
-    .getText()
-  await driver.quit()
+async function launchQuickProfile() {
+  let driver = await new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(new chrome.Options())
+    .build()
 
-  return `${city}: ${temperature}°C, ${condition}`
-}
+  try {
+    // 1. Go to the Multilogin login page
+    await driver.get(MULTILOGIN_URL)
 
-async function main() {
-  let cities = ['London', 'Tokyo', 'Washington D.C.', 'Canberra', 'Brasília']
-  let weatherResults = []
+    //  // 2. Wait for the login page to load and enter credentials
+    await driver.wait(
+      until.elementLocated(
+        By.className(
+          'cds--text-input cds--text-input--md cds--layout--size-md ng-untouched ng-pristine ng-invalid ng-star-inserted'
+        )
+      ),
+      5000
+    )
+    await driver
+      .findElement(
+        By.className(
+          'cds--text-input cds--text-input--md cds--layout--size-md ng-untouched ng-pristine ng-invalid ng-star-inserted'
+        )
+      )
+      .sendKeys(USERNAME)
+    await driver
+      .findElement(
+        By.className(
+          'ng-untouched ng-pristine ng-valid cds--text-input cds--text-input--md cds--layout--size-md ng-star-inserted'
+        )
+      )
+      .sendKeys(PASSWORD)
+    await driver.findElement(By.className('sign-in-form-cta')).click() // Click the login button
+    //
+    // 3. Wait for the homepage to load after login
+    await driver.wait(until.urlContains('/home/profiles'), 5000)
 
-  for (let city of cities) {
-    let weather = await getWeather(city)
-    weatherResults.push(weather)
+    // 4. Find and click the "Quick Profile" button
+    await driver.wait(
+      until.elementLocated(
+        By.className('cds--btn cds--btn--tertiary ng-star-inserted')
+      ),
+      5000
+    )
+    const quickProfileButton = await driver
+      .findElement(By.className('cds--btn cds--btn--tertiary ng-star-inserted'))
+      .click()
+
+    // 5. Wait for the Quick Profile to be created/launched
+    console.log('Quick Profile launched.')
+    await sleep(5000) // Wait for 5 seconds to ensure profile is created
+    await driver.wait(
+      until.elementLocated(By.className('cds--btn cds--btn--ghost')),
+      5000
+    )
+    await driver.findElement(By.className('cds--btn cds--btn--ghost')).click()
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
+  } finally {
+    // 6. Close the browser
+    await driver.quit()
   }
-
-  console.log(weatherResults)
 }
 
-main()
+// Run the script
+launchQuickProfile()
